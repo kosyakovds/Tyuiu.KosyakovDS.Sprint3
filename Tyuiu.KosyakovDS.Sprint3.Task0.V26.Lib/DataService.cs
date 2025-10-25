81b149877e3e63ffeff702fc07d289c95d2ee00e
kosyakovds <kosyakov27@mail.ru>
2025-10-25 17:43:50 +0500

    Add project files.

---
+++ Tyuiu.KosyakovDS.Sprint3.Task0.V26.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.KosyakovDS.Sprint3.Task0.V26.Lib
+{
+    public class DataService : ISprint3Task0V26
+    {
+        public double GetMultiplySeries(int startValue, int stopValue)
+        {
+
+            double p = 1;
+
+            for (int k = startValue; k <= stopValue; k++)
+            {
+                p *= (Math.Pow(2, k) / (k + 1)) * Math.Cos(1.8);
+            }
+            return p;
+
+        }
+    }
+}
---
+++ Tyuiu.KosyakovDS.Sprint3.Task0.V26/Program.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.KosyakovDS.Sprint3.Task0.V26.Lib;
+
+namespace Tyuiu.KosyakovDS.Sprint3.Task0.V26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("Введите х:");
+            double x = double.Parse(Console.ReadLine());
+
+            int startValue = 1;
+            int stopValue = 9;
+
+            double res = ds.GetMultiplySeries(startValue, stopValue);
+
+            Console.WriteLine(res);
+        }
+    }
+}
