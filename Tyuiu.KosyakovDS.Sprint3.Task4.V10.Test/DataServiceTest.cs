045bc0e5053da18e4907190b9cad26a3130b6e88
kosyakovds <kosyakov27@mail.ru>
2025-10-27 20:10:49 +0500

    Task4 V10

---
+++ Tyuiu.KosyakovDS.Sprint3.Task4.V10.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.KosyakovDS.Sprint3.Task4.V10.Lib;
+
+namespace Tyuiu.KosyakovDS.Sprint3.Task4.V10.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckValid()
+        {
+            DataService ds = new DataService();
+
+            double wait = -0.117;
+            double res = ds.Calculate(-5, 5);
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KosyakovDS.Sprint3.Task4.V10/Program.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.KosyakovDS.Sprint3.Task4.V10.Lib;
+
+namespace Tyuiu.KosyakovDS.Sprint3.Task4.V10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("Введите первое значение:");
+            int startValue = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе значение:");
+            int stopValue = Int32.Parse(Console.ReadLine());
+
+            double res = ds.Calculate(startValue, stopValue);
+
+            Console.WriteLine("Ваш результат: " + res);
+        }
+    }
+}
